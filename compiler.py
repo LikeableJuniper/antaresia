155a548a8e9a64a8e8c6fe2b0261de93da08b167
LikeableJuniper <noa_steffen@hotmail.com>
2024-09-12 08:56:03 +0200

    improved interpretation functionality

--- compiler.py
+++ compiler.py
@@ -6,10 +6,14 @@ instantCompile = False
 
 #the base code structure for any C code
 cCode = """#include <stdio.h>
-int main() {"""
+int main() {
+"""
 
 #dictionary assigning atr functions to their C counterparts
 atrCommands = {"print": "printf"}
+
+#list containing all assigned variable names
+definedVariables = []
 
 
 def interpret(code: list[str], indent=0) -> list[str]:
@@ -18,27 +22,34 @@ def interpret(code: list[str], indent=0) -> list[str]:
     indent can be set to automatically indent the generated C code. Useful when calling recursively, for example in nested loops.
     """
     finalCode = []
-    for i, line in code:
-        finalCode.append("")
+    for lineIndex, line in enumerate(code):
+        finalCode.append(" "*indent)
         splitLine = line.split(" ")
         for i in range(len(splitLine)):
             currentCommand = splitLine[i]
             if currentCommand in atrCommands.keys():
-                finalCode[i] += atrCommands[currentCommand] + "(" #add the translated C command and the opening bracket
+                finalCode[lineIndex] += atrCommands[currentCommand] + "(" #add the translated C command and the opening bracket
                 value = line.replace(")", "(").split("(")[1] #get the value of the function
                 if not value.startswith("\""): #allow other variable types than str to be printed as values
                     value = "\"" + value + "\""
-                finalCode[i] += value + ")"
-            finalCode[i] += ";"
+                finalCode[lineIndex] += value + ")"
+        
+        print(finalCode[lineIndex], len(finalCode[lineIndex]))
+        if len(finalCode[lineIndex].replace(" ", "")): #if there was no (translatable) content on the line, do not add semicolon
+            finalCode[lineIndex] += ";"
+        finalCode[lineIndex] += "\n"
+    
+    return finalCode
 
 
 with open("main.atr", "r") as f:
     lines = f.readlines()
-    interpretedLines = interpret(lines)
+    interpretedLines = interpret(lines, indent=4)
+    for line in interpretedLines:
+        cCode += line
         
 
-cCode += """
-return 0;
+cCode += """    return 0;
 }
 """
 
--- main.c
+++ main.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 int main() {
-    int a = 3;
-printf("3");
-return 0;
+    printf("3");
+    return 0;
 }
